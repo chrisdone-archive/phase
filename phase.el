cddf9fa40629865de65fc94eb4d96c3ae73a4456
Chris Done <chrisdone@gmail.com>
2016-11-14 17:27:10 +0000

    Awful but demonstrates the concept

--- phase.el
+++ phase.el
@@ -1,4 +1,4 @@
-;;; phase.el --- Phase service
+;;; phase.el --- Phase
 
 ;; Copyright (c) 2016 Chris Done. All rights reserved.
 
@@ -17,6 +17,113 @@
 
 ;;; Code:
 
+(require 'websocket)
 
+(setq websocket-debug t)
+
+(defvar phase-socket
+  (websocket-server
+   2016
+   :host 'local
+   :on-message 'phase-on-message
+   :on-open 'phase-on-open
+   :on-close 'phase-on-close))
+
+(defvar phase-client nil)
+
+(defvar phase-buffer (get-buffer "phase.el"))
+
+(defun phase-on-open (socket)
+  (setq phase-client socket)
+  (let ((output
+         (concat
+          "{"
+          "\"type\": \"faces\", \"faces\": "
+          (json-encode-array
+           (apply #'append ;; anywqay
+                  (mapcar (lambda (face)
+                            (list face
+                                  (face-foreground face nil 'default)))
+                          (face-list))))
+          "}")))
+    (websocket-send-text
+     socket
+     )))
+
+(defun phase-on-close (socket)
+  )
+
+(defun phase-on-message (socket frame)
+  (message "Received: %S" (websocket-frame-text frame))
+  (with-current-buffer phase-buffer
+    (phase-send-region
+     socket
+     (current-buffer)
+     0
+     (save-excursion (goto-char (point-max))
+                     (line-number-at-pos)))))
+
+(defvar-local phase-change-lines nil)
+(add-hook 'before-change-functions 'phase-before-change-function) ;; TODO: just local atm
+(defun phase-before-change-function (beg end)
+  (when (eq (current-buffer) phase-buffer)
+    (setq phase-change-lines
+          (cons (save-excursion (goto-char beg) (line-number-at-pos))
+                (save-excursion (goto-char end) (line-number-at-pos))))))
+
+(add-hook 'after-change-functions 'phase-after-change-function) ;; TODO: just local atm
+(defun phase-after-change-function (beg end _)
+  (when (eq (current-buffer) phase-buffer)
+    (let ((start-line (car phase-change-lines))
+          (end-line (cdr phase-change-lines)))
+      (phase-send-region phase-client (current-buffer) start-line end-line))))
+
+(defun phase-send-region (socket buffer start-line end-line)
+  (websocket-send-text
+   socket
+   (concat
+    "{"
+    "\"type\": \"region\","
+    "\"beg\":" (number-to-string start-line) ","
+    "\"end\":" (number-to-string end-line) ","
+    "\"lines\": " (phase-region-json buffer start-line end-line)
+    "}")))
+
+(defun phase-region-json (buffer beg end)
+  (with-current-buffer buffer
+    (json-encode-array
+     (mapcar (lambda (line)
+               (list :text line :faces (phase-string-faces line)))
+             (split-string
+              (buffer-substring
+               (save-excursion
+                 (goto-char (point-min))
+                 (forward-line (1- beg))
+                 (line-beginning-position))
+               (save-excursion
+                 (goto-char (point-min))
+                 (forward-line (1- end))
+                 (line-end-position)))
+              "\n")))))
+
+(defun phase-string-faces (object)
+  (let ((point 0)
+        (point-max (length object))
+        (faces (list)))
+    (while (not (>= point point-max))
+      (let ((plist (text-properties-at point object))
+            (next-change
+             (or (next-property-change point object)
+                 point-max)))
+        (push point faces)
+        (push next-change faces) ;; hello world!
+        (push (get-text-property point 'face object) faces)
+        (setq point next-change)))
+    (reverse faces)))
+
+(defun foo ()
+  "what's going on?"
+  (case etc);; blah
+  )
 
 (provide 'phase)
--- phase.js
+++ phase.js
@@ -1,3 +1,58 @@
 $(function(){
-alert('yo');
+  var conn = new WebSocket('ws://127.0.0.1:2016/');
+  conn.onopen = function () {
+    conn.send('Ping');
+  };
+  conn.onerror = function (error) {
+    console.log('WebSocket Error ' + error);
+  };
+  conn.onmessage = function (e) {
+    var reply = JSON.parse(e.data);
+    if (reply['type'] == 'faces') {
+      var faces = reply.faces;
+      // console.log(faces);
+      var rules = [];
+      for (var i =0; i < faces.length; i+=2) {
+        rules.push('.face-' + faces[i] + ' { color: ' + faces[i+1] + '; }');
+      }
+      var style = document.createElement('style');
+      style.innerHTML = rules.join('\n');
+      document.body.appendChild(style);
+    } else if (reply['type'] == 'region') {
+      var buffer = document.getElementById('buffer');
+      var start = reply.beg;
+      var end = reply.end;
+      console.log('%d - %d',start,end);
+      var lines = reply.lines, len = lines.length;
+      var anchor = null;
+      for (var i = 0 ; i < buffer.children.length; i++) {
+        if (i+1 >= start && i+1 <= end) {
+          anchor = buffer.children[i+1];
+          buffer.removeChild(buffer.children[i]);
+        }
+      }
+      for (var i = 0; i < len; i++) {
+        var el = document.createElement('div');
+        var line = lines[i];
+        var text = line.text;
+        if (!text) {
+          el.innerHTML = '&nbsp;';
+        } else if (line.faces) {
+          var faces = line.faces;
+          for (var j = 0; j < faces.length; j++) {
+            var start = faces[j]; j++;
+            var end = faces[j]; j++;
+            var face = faces[j];
+            var e = document.createElement('span');
+            e.innerText = text.substring(start,end);
+            if(face) e.className = 'face-' + face;
+            el.appendChild(e);
+          }
+        } else {
+          el.innerText = text;
+        }
+        buffer.insertBefore(el,anchor);
+      }
+    }
+  };
 });
